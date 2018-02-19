7427c902dc0ee9fdfab34cf44a943d75b4404869
Alejandro Nanez <alejonanez@gmail.com>
2018-02-19 16:32:24 -0500

    add docs for palindrome permutation

--- src/palindromePermutation/index.js
+++ src/palindromePermutation/index.js
@@ -1,3 +1,15 @@
+/**
+ * Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or prhease that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+ *
+ * Example.
+ *
+ * palindromePermutation('Tact Coa') -> true
+ * possible permutations: 'taco cat', 'atco cta', etc
+ *
+ * CTCI - Strings #1.4
+ *
+ * My own solution
+*/
 function hasher(string) {
   return string
     .split('')
